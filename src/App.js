0b2c2dff5c05b25fbb6bdd96cc39f058cff989d3
minhazu-cmyk <minhajabedin341@gmail.com>
2020-08-31 11:36:47 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,42 @@ import React from 'react';
 import './App.css';
 import Header from './component/Header/Header'
 import Shop from './component/shop/Shop';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Order from './component/order/Order';
+import Manage from './component/manage/Manage';
+import NotFound from './component/notfound/NotFound';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      <Header></Header>
-      <Shop></Shop>
-      </header>
-    </div>
+    <div>
+   <Header></Header>
+   <Router>
+   
+     <Switch>
+       <Route path="/shop">
+         <Shop></Shop>
+ </Route>
+ <Route path="/order">
+ <Order></Order>
+ </Route>
+ <Route path="/manage">
+<Manage></Manage>
+ </Route>
+ <Route exact path="/">
+<Shop></Shop>
+ </Route>
+
+<Route path="*">
+<NotFound></NotFound>
+</Route>
+
+     </Switch>
+   </Router>
+   </div>
   );
 }
 
---
+++ src/component/manage/Manage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Manage = () => {
+    return (
+        <div>
+          <h2> manage is comming soon</h2>  
+        </div>
+    );
+};
+
+export default Manage;
